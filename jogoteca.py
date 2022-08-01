046c4b5bf8ac687996bdcc3cce766598fe6fb644
Matheus Neves <matheus.almn@gmail.com>
2022-08-01 09:28:29 -0300

    Let's continue!

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,18 +6,32 @@ class Jogo:
         self.categoria=categoria
         self.console=console
 
-
+jogo1= Jogo('Skyrim', 'RPG', 'PC, PS4, XBOX')
+jogo2= Jogo('Genshin Impact', 'Gacha', 'PC, Mobile')
+jogo3= Jogo('League of Legends', "MOBA", "PC")
+lista = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
 
-@app.route('/inicio')
-def ola():
-    jogo1= Jogo('Skyrim', 'RPG', 'PC, PS4, XBOX')
-    jogo2= Jogo('Genshin Impact', 'Gacha', 'PC, Mobile')
-    jogo3= Jogo('League of Legends', "MOBA", "PC")
-    lista = [jogo1, jogo2, jogo3]
-
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Lista de Jogos', jogos=lista)
 
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Adicione um novo jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+ 
+
+
+app.run(debug=True)
